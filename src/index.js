e4a04081ba1a4e2564c4d273e8a65a10ce07f386
Rahmona Henry <email": null>
2016-04-16 14:57:21 +1200

    Complete homepage and show beers and beer profile pages

--- src/index.js
+++ src/index.js
@@ -2,21 +2,40 @@ var allBeers = require('../views/allBeers.hbs');
 var beerProfile = require('../views/beerProfile.hbs');
 var handlebars = require('handlebars');
 var request = require('superagent');
-var $ = require('jQuery');
+var $ = require('jquery');
 
-request
-.get('http://localhost:3000/api/v1/beers')
-.end(function(err, res){
 
-  document.body.innerHTML = allBeers({ beers: res.body });
-  $('.myBtn').click(function(event){
+$(document).ready(function(){
+  $('#showBtn, #backBtn').click(function () {
+  showAllBeers()
+  }) // close listener
+}) // close document ready
+// close homePage function()
 
-    request
-    .get('http://localhost:3000/api/v1/beers/'+$(this).data('id'))
-    .end(function(err, res){
-      $('#profile').html(beerProfile(res.body))
 
-    });
-  })
+function showAllBeers() {
+  request
+  .get('http://localhost:3000/api/v1/beers')
+  .end(function(err, res){
+    document.body.innerHTML = allBeers({ beers: res.body });
+    $('.myBtn').click(function(){
+      // console.log('this is this: ', this, 'this is $this', $(this))
+      getAndShowProfile($(this).data("id"))
+    }) // close click listener
+  }) // close end
+}
 
-});
+function getAndShowProfile(id) {
+  request
+  .get('http://localhost:3000/api/v1/beers/'+id)
+  .end(function(err, res){
+    var htmlFromTemplate = beerProfile(res.body)
+    // $('body').html(htmlFromTemplate)
+    document.body.innerHTML = htmlFromTemplate;
+    $('#backBtn').click(function(){
+      // console.log('back button')
+      showAllBeers()
+    })
+  });
+
+}
