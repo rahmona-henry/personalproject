759213ed7099d181d582af6d4e5dc0af5cc8f4f6
Rahmona Henry <email": null>
2016-04-17 15:21:37 +1200

    Attempt to read and write to file

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 var beers = require('./beers.js');
+var fs = require('fs');
+var bodyParser = require('body-parser');
 
 
 app.use(express.static('client'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'hbs');
 
 app.get('/', function(req, res) {
@@ -11,17 +15,22 @@ app.get('/', function(req, res) {
 });
 
 app.get('/beers', function (req, res){
-  res.json(beers)
+  fs.readFile('beers.js', 'utf8', function(err,data){
+    if (err) {
+      console.log ('error')
+    }
+    res.json(JSON.parse(data))
+  })
 });
 
 app.get('/beers/:id', function (req, res){
   res.json(beers[req.params.id-1])
-})
+});
 
 
 app.get('/new', function (req, res){
   res.render('addNewBeerProfile')
-})
+});
 
 
 app.listen(3000, function () {
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ var addNewBeer = require('../views/addNewBeerProfile.hbs');
 var handlebars = require('handlebars');
 var request = require('superagent');
 var $ = require('jquery');
+var fs = require('fs');
+var bodyParser = require('body-parser');
 
 
 $(document).ready(function(){
@@ -40,31 +42,32 @@ function getAndShowProfile(id) {
   }) // close end
 } //close function getAndShowProfile
 
-// function createNewBeer(givenName, givenDate, givenStyle, givenRecipe) {
-//   var newBeer = {}
-//   newBeer.name = givenName
-//   newBeer.date = givenDate
-//   newBeer.style = givenStyle
-//   newBeer.recipe = givenRecipe
-//   return newBeer
-// }
-
-// fs.readFile('beer.js', utf8, function (err,data){
-
-//   var dataObject = JSON.parse(data)
-//   var obj = createNewBeer(req.body[0].value, req.body[1].value, req.body[2].value, req.body[3].value)
-
-//   fs.writeFile('beer.js', JSON.stringify(dataToSave),function(err){
-//     if (err) {
-//       console.log('oh nooo error')
-//     } //close if
-//     else{
-//       console.log('ok')
-//     } //close else
-//   }) //close fswrite )}
-// }) //close fs.read )}
 
 
+fs.readFile('beer.js', utf8, function (err,data){
+
+  var dataObject = JSON.parse(data)
+  var beer = createNewBeer(req.body[0].value, req.body[1].value, req.body[2].value, req.body[3].value)
+  var dataToSave = dataObject.concat(beer)
+
+  fs.writeFile('beer.js', JSON.stringify(dataToSave),function(err){
+    if (err) {
+      console.log('oh nooo error')
+    } //close if
+    else{
+      res.send('ok')
+    } //close else
+  }) //close fswrite )}
+}) //close fs.read )}
+
+function createNewBeer(givenName, givenDate, givenStyle, givenRecipe) {
+  var newBeer = {}
+  newBeer.name = givenName
+  newBeer.date = givenDate
+  newBeer.style = givenStyle
+  newBeer.recipe = givenRecipe
+  return newBeer
+}
 
 
 
