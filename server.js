3a98d02ff933e4cec4b8f4ad0752e5e8501c83e9
Rahmona Henry <email": null>
2016-04-19 18:28:34 +1200

    Completed getting the data form req.body

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ var bodyParser = require('body-parser')
 app.use(express.static('client'));
 app.set('view engine', 'hbs');
 
+var knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: '.dev.sqlite3'
+  },
+  useNullAsDefault: true
+})
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
@@ -27,8 +35,20 @@ app.get('/new', function (req, res){
 })
 
 app.post('/foo', function (req, res){
-  console.log(req.body)
-  debugger
+  // console.log(req.body) //grab the data we are posting to this url and save it to a variable
+  var data = req.body
+
+  //then we want to create a knex statement, to put that data into the database
+  // knex.(DO SOMETHING) hint: insert
+  knex('beer').insert({name:data.name},{style:data.style},{inventory:data.inventory})
+  // console.log('data-name:', data.name)
+  // console.log('data-style:', data.style)
+  // console.log('data-inventory:', data.inventory)
+
+  // work out where you want to go next, weather its rendering a new pages saying 'tahnks' or the berrs page, or a specific beer page
+  res.send('success')
+
+
 })
 
 
@@ -43,6 +63,6 @@ app.listen(3000, function () {
 // install knex -- DONE
 // create a migration - which creates a new table - with above info --DONE
 // create a seed file - with test data --DONE
-// go back to server - create a insert statment taht inserts info into db
+// go back to server - create a insert statment that inserts info into db
 // redirect - to that the specfic page - that you want
 
