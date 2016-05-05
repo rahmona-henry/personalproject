f95aca437c09f0eed601c1fbb19e8d3dc3bf30bc
Rahmona Henry <email": null>
2016-05-05 18:31:50 +1200

    Append Ul function wip

--- views/beerList.js
+++ views/beerList.js
@@ -1,11 +1,11 @@
 module.exports = function(beers, selector){
 
-  //(var i=0; i<beers.length; i++){
+  //for(var i=0; i<beers.length; i++){
     console.log(beers)
-      var rootElement = document.querySelector(selector)
-      var ul = document.createElement('ul')
-      rootElement.appendChild(ul)
- //}
+    var rootElement = document.querySelector(selector)
+    var ul = document.createElement('ul')
+    rootElement.appendChild(ul)
+  }
 
 
-}
+//}
