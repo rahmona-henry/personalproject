2063c76c5168b0901dae563ae231027264d1c604
Rahmona Henry <email": null>
2016-05-09 17:58:06 +1200

    Fix syntax error in seed file

--- seeds/beer.js
+++ seeds/beer.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     knex('beer').del(),
 
     // Inserts seed entries
-    knex('beer').insert({name:'Grogans', style: 'stout', inventory:23},
+    knex('beer').insert({name:'Grogans', style: 'stout', inventory:23}),
     knex('beer').insert({name: 'Seans', style: 'stout', inventory:46}),
     knex('beer').insert({name: 'Gerties', style: 'stout', inventory:72})
   );
