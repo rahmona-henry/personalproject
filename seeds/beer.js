de915b91dfd3ff9ec59eb5bf0f99b97c8ac63990
Rahmona Henry <email": null>
2016-05-10 16:40:45 +1200

    plays with seed file

--- seeds/beer.js
+++ seeds/beer.js
@@ -5,8 +5,11 @@ exports.seed = function(knex, Promise) {
     knex('beer').del(),
 
     // Inserts seed entries
-    knex('beer').insert({name:'Cara', style: 'American Pale Ale', inventory:48, brewDate:'23 Jan 2016', bottlingDate:'23 Feb 2016', ingredients: 'bhhhghfhffgfgfgfgfg', process: 'ghfgdhfhdfghdgfhdgf', tasting: 'dsfsfsfdfdfd'}),
-    knex('beer').insert({name: 'Zeus', style: 'Pilsner', inventory:46, brewDate:'23 Jan 2016', bottlingDate:'23 Feb 2016', ingredients: 'bhhhghfhffgfgfgfgfg', process: 'ghfgdhfhdfghdgfhdgf', tasting: 'dsfsfsfdfdfd'}),
-    knex('beer').insert({name: 'Sheebie', style: 'Pale Ale', inventory:72, brewDate: '23 Jan 2016', bottlingDate:'23 Feb 2016', ingredients: 'bhhhghfhffgfgfgfgfg', process: 'ghfgdhfhdfghdgfhdgf', tasting: 'dsfsfsfdfdfd'})
+    knex('beer').insert({name:'Cara', style: 'American Pale Ale', inventory:48, brewDate:'23 Jan 2016'})
   );
 };
+
+
+// knex('beer').insert({name:'Cara', style: 'American Pale Ale', inventory:48, brewDate:'23 Jan 2016', bottlingDate:'23 Feb 2016', ingredients: 'bhhhghfhffgfgfgfgfg', process: 'ghfgdhfhdfghdgfhdgf', tasting: 'dsfsfsfdfdfd'}),
+// knex('beer').insert({name: 'Zeus', style: 'Pilsner', inventory:46, brewDate:'23 Jan 2016', bottlingDate:'23 Feb 2016', ingredients: 'bhhhghfhffgfgfgfgfg', process: 'ghfgdhfhdfghdgfhdgf', tasting: 'dsfsfsfdfdfd'}),
+// knex('beer').insert({name: 'Sheebie', style: 'Pale Ale', inventory:72, brewDate: '23 Jan 2016', bottlingDate:'23 Feb 2016', ingredients: 'bhhhghfhffgfgfgfgfg', process: 'ghfgdhfhdfghdgfhdgf', tasting: 'dsfsfsfdfdfd'})
